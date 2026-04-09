01e5d93d684d2242a164c092386ded31c61cc2d1
Joseph Morales <145137963+moejora@users.noreply.github.com>
2026-04-09 15:16:07 +0000

    test again liking animations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mora-ledger-v4';
+const CACHE = 'mora-ledger-v5';
 const ASSETS = [
   '/mora-ledger/',
   '/mora-ledger/index.html',
@@ -6,41 +6,20 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
   'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;500;600&display=swap'
 ];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  const isLocal = url.origin === self.location.origin;
-  if (isLocal) {
-    e.respondWith(
-      caches.match(e.request).then(cached => {
-        if (cached) return cached;
-        return fetch(e.request).then(res => {
-          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-          return res;
-        });
-      })
-    );
+  const isLocal = new URL(e.request.url).origin === self.location.origin;
+  if(isLocal){
+    e.respondWith(caches.match(e.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res;});
+    }));
   } else {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => { caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res; })
-        .catch(() => caches.match(e.request))
-    );
+    e.respondWith(fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res;}).catch(()=>caches.match(e.request)));
   }
 });
